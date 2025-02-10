1e48264093436724516a407a926d5abef3aca3e1
Youj33n <64790435+youj33ns@users.noreply.github.com>
2025-02-10 22:03:52 +0300

    Update json.php

--- json.php
+++ json.php
@@ -1,21 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    print "привет";
-    // на какие данные рассчитан этот скрипт
-    header("Content-Type: application/json");
-    // разбираем JSON-строку на составляющие встроенной командой
-    $data = json_decode(file_get_contents("php://input"));
-    // отправляем в ответ строку с подтверждением
-    echo "Сервер получил следующие данные: имя — $data->hello";
-    ?>
-</body>
-</html>
+<?php
 
+print "привет";
 
+// на какие данные рассчитан этот скрипт
+header("Content-Type: application/json");
+// разбираем JSON-строку на составляющие встроенной командой
+$data = json_decode(file_get_contents("php://input"));
+print "foo — это $data";
+// отправляем в ответ строку с подтверждением
+echo "Сервер получил следующие данные: имя — $data->hello";
+?>
