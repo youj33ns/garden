60569ac54166f7c1062fe12bb89d4a1fe5c4d92a
Youj33n <64790435+youj33ns@users.noreply.github.com>
2025-02-10 21:10:46 +0300

    Create json.php

---
+++ json.php
@@ -0,0 +1,7 @@
+<?php
+// на какие данные рассчитан этот скрипт
+header("Content-Type: application/json");
+// разбираем JSON-строку на составляющие встроенной командой
+$data = json_decode(file_get_contents("php://input"));
+// отправляем в ответ строку с подтверждением
+echo "Сервер получил следующие данные: имя — $data->article, фамилия — $data->price";
