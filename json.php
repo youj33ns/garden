0c7885f929f7d78d488291d1d29780b2def78e07
Youj33n <64790435+youj33ns@users.noreply.github.com>
2025-02-10 22:06:00 +0300

    Update json.php

--- json.php
+++ json.php
@@ -1,12 +1,8 @@
 <?php
-
-print "привет";
-
 // на какие данные рассчитан этот скрипт
 header("Content-Type: application/json");
 // разбираем JSON-строку на составляющие встроенной командой
 $data = json_decode(file_get_contents("php://input"));
-print "foo — это $data";
 // отправляем в ответ строку с подтверждением
 echo "Сервер получил следующие данные: имя — $data->hello";
 ?>
